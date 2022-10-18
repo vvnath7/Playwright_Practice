82599bc3099f4dd632d38df772f72d1572116f17
VARUN <varun89@live.in>
2022-10-18 00:11:06 -0400

    added beforeEach hook

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -1,8 +1,15 @@
-import { test, expect, chromium } from '@playwright/test';
+import { test, expect, chromium, Page } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://playwright.dev');
+});
 
 test('basic test', async ({ page }) => {
-    await page.goto('https://playwright.dev');
-    const title = page.title;
     await page.click("text=Get started");
     await expect(page.locator('//*[@id="__docusaurus"]/div[2]/div/main/div/div/div[1]/div/article/div[2]/header/h1')).toHaveText('Installation');
+  });
+
+  test('basic test 2', async ({ page }) => {
+   await page.click("text=API");
+   await expect(page.locator('//*[@id="__docusaurus"]/div[2]/div/main/div/div/div[1]/div/article/div[2]/header/h1')).toHaveText('Playwright Library');
   });
