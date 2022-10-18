e73435ac27b071f7e0da9dd6197ea86378ad3356
VARUN <varun89@live.in>
2022-10-17 23:55:48 -0400

    Updated test case

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -1,7 +1,8 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-    await page.goto('https://ndtv.com');
+    await page.goto('https://playwright.dev');
     const title = page.title;
-    await expect(title).toBe("Google1");
+    await page.click("text=Get started");
+    await expect(page.locator('//*[@id="__docusaurus"]/div[2]/div/main/div/div/div[1]/div/article/div[2]/header/h1')).toHaveText('Installation');
   });
